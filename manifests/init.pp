8b2c5c4d44926e1a529f1c66d160ad4bace2b69e
andreas <andreas@immerda.ch>
2008-10-15 13:24:15 +0000

    include ibp::opt::bin

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,6 +24,7 @@
 class motd { }
 
 class motd::client {
+    include ibp::opt::bin
     file{"/etc/motd":
         content => generate("/opt/bin/motd_gen.sh", "${hostname}", "${motd_message}"),
         owner => root, group => 0, mode => 0644;
