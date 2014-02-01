b7efdde659b9d4549a2afb7e8c3bbfb180e9ffdd
mh <mh@immerda.ch>
2014-02-01 16:26:21 +0100

    linting

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,13 +1,14 @@
+# manage dependencies for figlet
 class motd::puppetmaster {
-  package{figlet:
+  package{'figlet':
     ensure => installed,
-    before => File["/etc/motd"],
   }
 
-  file{"/opt/bin/motd_gen.sh":
-    source => "puppet:///modules/motd/motd_gen.sh",
-    before => File["/etc/motd"],
-    require => [ Package[figlet], File["/opt/bin"] ],
-    owner => puppet, group => 0, mode => 0755;
+  file{'/opt/bin/motd_gen.sh':
+    source  => 'puppet:///modules/motd/motd_gen.sh',
+    require => Package['figlet'],
+    owner   => root,
+    group   => 0,
+    mode    => '0755';
   }
 }
