eb2bc1f0a72505d48d2a8b3317f482a549e821d7
tr <tr@subpage.ch>
2014-05-02 13:59:36 +0200

    change char to us-ascii

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 # motd module
 #
 # Copyright 2008, Puzzle ITC GmbH
-# Marcel Härry haerry+puppet(at)puzzle.ch
+# Marcel Haerry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
 #
 # This program is free software; you can redistribute 
