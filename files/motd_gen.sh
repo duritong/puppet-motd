b72eef6daca3df0296ba566bb474d60bf43ef93c
mh <mh@immerda.ch>
2022-02-09 22:39:48 +0100

    not yet available in EL9

--- files/motd_gen.sh
+++ files/motd_gen.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-/usr/bin/figlet $1
+if [ -x /usr/bin/figlet ]; then
+  /usr/bin/figlet $1
+else
+  /bin/echo -e "$1"
+  /bin/echo "---"
+fi
 /bin/echo -e "$2"
--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,11 +1,17 @@
 # manage dependencies for figlet
 class motd::puppetmaster {
-  package{'figlet':
-    ensure => installed,
-  } -> file{'/usr/local/bin/motd_gen.sh':
-    source  => 'puppet:///modules/motd/motd_gen.sh',
-    owner   => root,
-    group   => 0,
-    mode    => '0755';
+
+  # https://bugzilla.redhat.com/show_bug.cgi?id=2032196
+  if versioncmp($facts['os']['release']['major'],'9') < 0 {
+    package{'figlet':
+      ensure => installed,
+      before => File['/usr/local/bin/motd_gen.sh'],
+    }
+  }
+  file{'/usr/local/bin/motd_gen.sh':
+    source => 'puppet:///modules/motd/motd_gen.sh',
+    owner  => root,
+    group  => 0,
+    mode   => '0755',
   }
 }
